2bfa0064bbe8ac9c3ddf8678a9028be251f3ef3a
yury <yuriy-t87@yandex.ru>
2022-10-06 13:37:47 +0300

    Третья задача

---
+++ ex3.py
@@ -0,0 +1,14 @@
+#Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
+
+def get_sum(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += (1 + 1 / i)**i
+    return sum
+
+
+
+num = int(input('Введите целое число: '))
+
+print(f'{num} => {get_sum(num)}')
+
